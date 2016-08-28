388e9e494a848e06de265d371f504c3cecf91aa7
Emil Rosenius Pedersen <emil0srp@gmail.com>
2016-08-28 14:24:52 +0200

    added some other random shit

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-var hej = 12;
-console.log(hej);
+var numbers = {
+    small: [1, 2, 3, 4, 5, 6, 7],
+    medium: [54, 54, 54, 54, 23, 612, 712],
+    large: [1214214, 2124124, 3122112, 4412441, 541221, 6124124, 7124124]
+};
 
-var numbers = [1, 2, 3, 4, 5, 6, 7];
+numbers.small.map(function (num) {
+    console.log(num);
+});
 
-numbers.map(function (num) {
-    if (num > 3)
-        console.log(num);
+numbers.medium.map(function (num) {
+    console.log(num);
+});
+
+numbers.large.map(function (num) {
+    console.log(num);
 });
 
 
